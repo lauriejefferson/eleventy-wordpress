523d7f5d1cb0b2c71614cc731ce95eac11531908
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2024-06-12 16:41:29 -0400

    adds pages global data file

---
+++ src/_data/pages.js
@@ -0,0 +1,7 @@
+const fetch = require('node-fetch');
+
+module.exports = async function () {
+  return fetch(`${process.env.CMS}/wp-json/wp/v2/pages`)
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+};
