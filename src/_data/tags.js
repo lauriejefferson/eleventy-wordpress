9ebe7cea72af76db23db3e253bdf3be4f0aa58da
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2024-06-13 17:23:41 -0400

    adds fetch requests for post, pages, and tags global data

--- src/_data/tags.js
+++ src/_data/tags.js
@@ -1,8 +1,7 @@
 const fetch = require('node-fetch');
-require('dotenv').config();
 
 module.exports = async function () {
-  return fetch(`${process.env.CMS}/wp-json/wp/v2/posts/`, { method: 'GET' })
+  return fetch(`${process.env.CMS}/wp-json/wp/v2/tags`)
     .then((res) => res.json())
     .then((json) => console.log(json));
 };
