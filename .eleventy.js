36b5d8ea5a988eade7c941d51f60d6125d4da7d3
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2024-06-15 18:07:38 -0400

    adds content to index and post pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,26 @@
+const markdownIt = require('markdown-it');
+const util = require('util');
+const { DateTime } = require('luxon');
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('./src/css');
+  eleventyConfig.addFilter('md', function (content = '') {
+    return markdownIt({ html: true }).render(content);
+  });
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toLocaleString(
+      DateTime.DATE_MED
+    );
+  });
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    excerpt_separator: '<!--excerpt-->',
+  });
+  eleventyConfig.addCollection('recentPosts', function (collection) {
+    return collection.getFilteredByTag('post').reverse().slice(0, 2);
+  });
+  eleventyConfig.addFilter('dump', (obj) => {
+    return util.inspect(obj);
+  });
   return {
     dir: {
       input: 'src',
