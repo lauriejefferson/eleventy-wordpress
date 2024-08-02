a25ef76e9bb92664aa396033c88eee193db5d839
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2024-08-02 09:22:40 -0400

    wraps title and paragraph on about-me page in a container

--- src/_data/pages.js
+++ src/_data/pages.js
@@ -2,7 +2,9 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 module.exports = async function () {
-  return fetch(`${process.env.CMS}/wp-json/wp/v2/pages`)
+  return fetch(
+    `${process.env.CMS}/wp-json/wp/v2/pages?_fields=id,title,date,slug,content,tags,acf&per_page=100`
+  )
     .then((res) => res.json())
     .then((json) => json);
 };
